3326c36fafe097c3ad486b2b8c67bef21e856f5e
Neelam123K <153060910+Neelam123K@users.noreply.github.com>
2025-07-23 16:31:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function getFormvalue() {
+function getFormvalue(event) {
     //Write your code here
-
+event.preventDefault();
 }
+const form = document.getElementById('form1');
+const fname = form.fname.value.trim();  
+const lname = form.lname.value.trim();
+
+if (!fname && !lname) {
+        alert("Please enter your first and last name.");
+        return false;
+      }
+
+      const fullName = `${fname} ${lname}`.trim(); // Handle case when one of them is empty
+      alert(fullName);
+      return false;
